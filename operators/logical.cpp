4a0d1e819492820cd2f4cc95ba7062ac8b239a19
Rampravesh <rampraveshkr4545@gmail.com>
2026-01-13 17:23:08 +0530

    logical operator in c++

---
+++ operators/logical.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    bool a = true;
+    bool b = false;
+
+    cout << "a && b is " << (a && b) << endl;
+    cout << "a || b is " << (a || b) << endl;
+    cout << "!a is " << (!a) << endl;
+    
+    return 0;
+}
