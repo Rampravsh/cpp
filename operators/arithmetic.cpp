ec1a743a92ec633a95886b0874b6298d2fab3df9
Rampravesh <rampraveshkr4545@gmail.com>
2026-01-13 15:50:42 +0530

    some operation in c++

---
+++ operators/arithmetic.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Declare two integer variables
+    int a = 10;
+    int b = 5;
+
+    // Addition
+    cout << "a + b = " << (a + b) << endl;
+
+    // Subtraction
+    cout << "a - b = " << (a - b) << endl;
+
+    // Multiplication
+    cout << "a * b = " << (a * b) << endl;
+
+    // Division
+    cout << "a / b = " << (a / b) << endl;
+
+    // Modulus
+    cout << "a % b = " << (a % b) << endl;
+
+    return 0;
+}
