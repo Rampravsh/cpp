61c3194e96dd110f04560539705577117a76dcea
Rampravesh <rampraveshkr4545@gmail.com>
2026-03-02 17:02:54 +0530

    dereference operator

--- Pointer/code.cpp
+++ Pointer/code.cpp
@@ -3,13 +3,15 @@ using namespace std;
 
 int main() {
     int a =10;
-    cout<< &a<<endl;
+    cout<< "&a: " << &a<<endl;
 
     int* ptr =&a;
-    cout<<ptr<<endl;
+    cout<< "ptr: " <<ptr<<endl;
 
     int** ptrOfptr=&ptr;
     cout<<"ptrOfptr: "<<ptrOfptr<<endl;
+
+    cout<<"*(&a): "<<*(&a)<<endl;
 
     float price= 100.25f;
     float*ptr2=&price;
