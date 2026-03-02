c4363570ec07d98d46d8eb93bd204c144eac04a6
Rampravesh <rampraveshkr4545@gmail.com>
2026-03-02 17:22:22 +0530

    null pointer

--- Pointer/code.cpp
+++ Pointer/code.cpp
@@ -2,19 +2,30 @@
 using namespace std;
 
 int main() {
-    int a =10;
-    cout<< "&a: " << &a<<endl;
+    // int a =10;
+    // cout<< "&a: " << &a<<endl;
 
-    int* ptr =&a;
-    cout<< "ptr: " <<ptr<<endl;
+    // int* ptr =&a;
+    // cout<< "ptr: " <<ptr<<endl;
 
-    int** ptrOfptr=&ptr;
-    cout<<"ptrOfptr: "<<ptrOfptr<<endl;
+    // int** ptrOfptr=&ptr;
+    // cout<<"ptrOfptr: "<<ptrOfptr<<endl;
 
-    cout<<"*(&a): "<<*(&a)<<endl;
+    // cout<<"*(&a): "<<*(&a)<<endl;
 
-    float price= 100.25f;
-    float*ptr2=&price;
-    cout <<"ptr2: " << ptr2 <<"\n&price: "<<&price<<endl;
+    // cout<<"*(ptr): "<<*(ptr)<<endl;
+
+    // cout<<"*(ptrOfptr): "<<*(ptrOfptr)<<endl;
+    // cout<<"**(ptrOfptr): "<<**(ptrOfptr)<<endl;
+
+    // float price= 100.25f;
+    // float*ptr2=&price;
+    // cout <<"ptr2: " << ptr2 <<"\n&price: "<<&price<<endl;
+
+
+    //NULL POINTER👍
+
+    int** ptr=NULL;
+    cout<<"ptr: " <<ptr<<endl;
     return 0;
 }
