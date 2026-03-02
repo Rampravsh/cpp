7a5062e390d47b8aab60395d25cd7c2b854aac3f
Rampravesh <rampraveshkr4545@gmail.com>
2026-03-02 19:15:43 +0530

    end of pointer

--- Pointer/code4.cpp
+++ Pointer/code4.cpp
@@ -6,15 +6,29 @@ int main()
     // pointer arithmetic
 
     int arr[] = {1, 2, 3, 4, 5};
-    int a = 10;
-    int *ptr = &a;
-    cout << ptr << endl;
-    //Increment(++) /decrement(--)
-    ptr++;
-    cout << ptr << endl;
-    //Add/Subtract Number
 
-    ptr+=4;
-    cout<<ptr<<endl;
+    // cout << *arr << endl;
+    // cout << *(arr + 1) << endl;
+    // cout << *(arr + 2) << endl;
+    // cout << *(arr + 3) << endl;
+    // cout << *(arr + 4) << endl;
+
+    // int a = 10;
+    // int *ptr = &a;
+    // cout << ptr << endl;
+    // //Increment(++) /decrement(--)
+    // ptr++;
+    // cout << ptr << endl;
+    // //Add/Subtract Number
+
+    // ptr+=4;
+    // cout<<ptr<<endl;
+
+
+    int*ptr2; //100
+    int*ptr1=ptr2+2; //108
+    cout <<ptr1-ptr2<<endl; //2
+    int*ptr3;
+    cout << ptr3-ptr2 << endl; //garbage value
     return 0;
 }
